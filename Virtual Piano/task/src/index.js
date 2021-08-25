03211e8e1db9d3b75fd0fa109abd702a2579eac9
khafizovas <justhomosapiens121@gmail.com>
2021-08-25 05:04:49 +0300

    add key press handler
    Signed-off-by: khafizovas <justhomosapiens121@gmail.com>

--- Virtual Piano/task/src/index.js
+++ Virtual Piano/task/src/index.js
@@ -1,1 +1,13 @@
 const ALLOWED_KEYS = ["A", "S", "D", "F", "G", "H", "J"];
+
+document.addEventListener("keypress", handleKeyPress);
+
+function handleKeyPress(e) {
+  const pressedKey = e.key.toUpperCase();
+
+  if (ALLOWED_KEYS.includes(pressedKey)) {
+    console.log(`The '${pressedKey}' key is pressed`);
+  } else {
+    console.log("Invalid key");
+  }
+}
