4a7d38df4a1730a70c8b12439c569d9eb34edd7b
khafizovas <justhomosapiens121@gmail.com>
2021-09-02 00:01:57 +0300

    add white keys sounds play
    Signed-off-by: khafizovas <justhomosapiens121@gmail.com>

--- Virtual Piano/task/src/index.js
+++ Virtual Piano/task/src/index.js
@@ -7,6 +7,7 @@ function handleKeyPress(e) {
 
   if (ALLOWED_KEYS.includes(pressedKey)) {
     console.log(`The '${pressedKey}' key is pressed`);
+    const keySound = new Audio(`white_keys/${pressedKey}.mp3`).play();
   } else {
     console.log("Invalid key");
   }
