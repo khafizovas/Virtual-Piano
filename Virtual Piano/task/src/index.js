d92caa01ce5266fa9b8b98c2dc1b474f9489ecfa
khafizovas <justhomosapiens121@gmail.com>
2021-09-02 22:33:44 +0300

    add black keys sounds
    Signed-off-by: khafizovas <justhomosapiens121@gmail.com>

--- Virtual Piano/task/src/index.js
+++ Virtual Piano/task/src/index.js
@@ -1,11 +1,14 @@
 document.addEventListener("keypress", handleKeyPress);
 
 function handleKeyPress(e) {
-  const ALLOWED_KEYS = ["A", "S", "D", "F", "G", "H", "J"];
+  const WHITE_KEYS = ["A", "S", "D", "F", "G", "H", "J"];
+  const BLACK_KEYS = ["W", "E", "T", "Y", "U"];
   const pressedKey = e.key.toUpperCase();
 
-  if (ALLOWED_KEYS.includes(pressedKey)) {
+  if (WHITE_KEYS.includes(pressedKey)) {
     new Audio(`white_keys/${pressedKey}.mp3`).play();
+  } else if (BLACK_KEYS.includes(pressedKey)) {
+    new Audio(`black_keys/${pressedKey}.mp3`).play();
   } else {
     console.log("Invalid key");
   }
