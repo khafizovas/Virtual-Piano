32636c15d6026464d479c988498a9364ce2090e7
khafizovas <justhomosapiens121@gmail.com>
2021-09-02 14:05:44 +0300

    refactor
    Signed-off-by: khafizovas <justhomosapiens121@gmail.com>

--- Virtual Piano/task/src/index.js
+++ Virtual Piano/task/src/index.js
@@ -1,13 +1,11 @@
-const ALLOWED_KEYS = ["A", "S", "D", "F", "G", "H", "J"];
-
 document.addEventListener("keypress", handleKeyPress);
 
 function handleKeyPress(e) {
+  const ALLOWED_KEYS = ["A", "S", "D", "F", "G", "H", "J"];
   const pressedKey = e.key.toUpperCase();
 
   if (ALLOWED_KEYS.includes(pressedKey)) {
-    console.log(`The '${pressedKey}' key is pressed`);
-    const keySound = new Audio(`white_keys/${pressedKey}.mp3`).play();
+    new Audio(`white_keys/${pressedKey}.mp3`).play();
   } else {
     console.log("Invalid key");
   }
